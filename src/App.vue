ca9617b7267de3daa668ee03ac052a6cb7342fa4
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2024-10-20 22:40:40 +0800

    feat: gas tracker

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,83 @@
-<template></template>
+<template>
+  <main>
+    <div class="hero">
+      <h1>{{ toFixed(gwei, 4) }} gwei</h1>
+      <p>updated @ {{ updatedAt ? formatISO(updatedAt) : "" }}</p>
+      <p>update in {{ countdown }}s</p>
+    </div>
+    <h2>Ethereum Gas Tracker</h2>
+    <p>
+      Unlike
+      <a
+        href="https://etherscan.io/gastracker"
+        target="_blank"
+        rel="noopener noreferrer"
+        >Etherscan</a
+      >, this Ethereum Gas Tracker directly retrieves gas prices through
+      <a
+        herf="https://ethereum.github.io/execution-apis/api-documentation/"
+        target="_blank"
+        rel="noopener noreferrer"
+        >Ethereum's JSON-RPC interface</a
+      >, provided by
+      <a
+        href="https://developers.cloudflare.com/web3/ethereum-gateway/"
+        target="_blank"
+        rel="noopener noreferrer"
+        >Cloudflare</a
+      >. It doesn't require an access token and is a static website that can be
+      deployed anywhere. The gas price will be updated in the webpage's title
+      bar every {{ interval }} seconds, allowing users to keep it open in the
+      background as an indicator.
+    </p>
+  </main>
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useInterval, useIntervalFn, useTitle } from "@vueuse/core";
+import { formatISO, secondsToMilliseconds } from "date-fns";
+import { Decimal } from "decimal.js";
+import { computed, onMounted, ref } from "vue";
 
-<style scoped></style>
+const wei = ref<null | Decimal>(null);
+const gwei = computed(() => (wei.value ? wei.value.div(10 ** 9) : null));
+
+const updatedAt = ref<null | Date>();
+const title = computed(
+  () => `${toFixed(gwei.value, 2)} | Ethereum gas tracker`,
+);
+useTitle(title);
+
+function toFixed(n: null | Decimal, m: number) {
+  return n ? n.toFixed(m) : null;
+}
+
+const endpoint = "https://cloudflare-eth.com";
+async function refreshGasPrice() {
+  const body = {
+    jsonrpc: "2.0",
+    method: "eth_gasPrice",
+    params: [],
+    id: 0,
+  };
+  const json = await fetch(endpoint, {
+    method: "POST",
+    body: JSON.stringify(body),
+  }).then((r) => r.json());
+  wei.value = new Decimal(json.result);
+  updatedAt.value = new Date();
+}
+
+const interval = 15;
+useIntervalFn(() => refreshGasPrice(), secondsToMilliseconds(interval));
+const counter = useInterval(secondsToMilliseconds(1));
+const countdown = computed(() => interval - (counter.value % interval));
+
+onMounted(() => refreshGasPrice());
+</script>
+
+<style scoped>
+.hero {
+  text-align: center;
+}
+</style>
